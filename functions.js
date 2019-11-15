f4de192815bc8fd6aeb51b18868764d9c56ee3df
ZROSENSOHN LAPTOP <zac@satprepct.com>
2019-11-14 19:26:41 -0500

    Added functions.js

---
+++ functions.js
@@ -0,0 +1,7 @@
+function cageimg() {
+    document.getElementById("cage").style.backgroundImage="url(img/Nic-Cage-Vampires-Kiss.jpg)";
+}
+
+function reset() {
+    document.getElementById("cage").style.backgroundImage="url(img/nic-1.jpg)";
+}
